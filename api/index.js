8c4ccf6c9e254859525eb243069146fae3a43aca
Hallo Robby <107454049+robbymugiakbar@users.noreply.github.com>
2025-02-22 19:26:53 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import cors from 'cors';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
