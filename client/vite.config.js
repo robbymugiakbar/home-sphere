246344f2faf0adc7e121df59cc380a0f083d41e1
Hallo Robby <107454049+robbymugiakbar@users.noreply.github.com>
2025-02-23 13:11:08 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        secure: false,
-    },
-  },
-},
-
   plugins: [react()],
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  }
 })
